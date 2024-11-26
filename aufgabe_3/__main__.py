349fc7f3e5f28c6354399b96d86f97b3ce6773ec
Gregoire <wi22b060@technikum-wien.at>
2024-11-27 00:03:11 +0100

    web application

--- aufgabe_3/__main__.py
+++ aufgabe_3/__main__.py
@@ -1,31 +1,78 @@
-"""An Azure RM Python Pulumi program"""
-
 import pulumi
-from pulumi_azure_native import storage
-from pulumi_azure_native import resources
+import os
+from pulumi_azure_native import resources, storage, web, insights
+from pulumi import FileAsset, Output
 
-# Create an Azure Resource Group
-resource_group = resources.ResourceGroup("resource_group")
+# **1. Ressourcengruppe erstellen**
+resource_group = resources.ResourceGroup("uebung3-resourcegroup", location="japaneast")
 
-# Create an Azure resource (Storage Account)
-account = storage.StorageAccount(
-    "sa",
-    resource_group_name=resource_group.name,
-    sku={
-        "name": storage.SkuName.STANDARD_LRS,
-    },
-    kind=storage.Kind.STORAGE_V2,
+# **2. Storage-Account erstellen**
+# wird benötigt zur speicherung von Daten
+storage_account = storage.StorageAccount(
+    "storageaccount",
+    resource_group_name=resource_group.name,  # Verknüpft mit der Ressourcengruppe
+    location=resource_group.location,  # Gleicher Standort wie die Ressourcengruppe
+    sku=storage.SkuArgs(name="Standard_LRS"),  # Standard Storage-Tarif
+    kind="StorageV2",  # Moderner Storage-Typ für allgemeine Nutzung
+    allow_blob_public_access=True  # Aktiviert öffentlichen Zugriff auf Blobs
 )
 
-# Export the primary key of the Storage Account
-primary_key = (
-    pulumi.Output.all(resource_group.name, account.name)
-    .apply(
-        lambda args: storage.list_storage_account_keys(
-            resource_group_name=args[0], account_name=args[1]
-        )
+# **3. Blob-Container erstellen**
+
+blob_container = storage.BlobContainer(
+    "blobcontainer",
+    account_name=storage_account.name,  # Verknüpft mit dem Storage-Account
+    resource_group_name=resource_group.name,  # Verknüpft mit der Ressourcengruppe
+    public_access="Blob"  # Ermöglicht öffentlichen Lesezugriff auf die Blobs
+)
+
+# **4. ZIP-Datei erstellen**
+os.system('cd clco-demo && zip -r ../webapp.zip .')
+
+# **5. ZIP-Datei in Blob-Container hochladen**
+app_blob = storage.Blob(
+    "webappzip",
+    resource_group_name=resource_group.name,  # Verknüpft mit der Ressourcengruppe
+    account_name=storage_account.name,  # Verknüpft mit dem Storage-Account
+    container_name=blob_container.name,  # Verknüpft mit dem Blob-Container
+    source=FileAsset("./webapp.zip")  
+)
+
+# **6. Blob-URL generieren**
+blob_url = pulumi.Output.concat(
+    "https://", storage_account.name, ".blob.core.windows.net/", blob_container.name, "/", app_blob.name
+)
+
+# **7. App Service Plan erstellen**
+app_service_plan = web.AppServicePlan(
+    "serviceplan",
+    resource_group_name=resource_group.name,  # Verknüpft mit der Ressourcengruppe
+    location=resource_group.location,  # Standort der Ressourcengruppe
+    kind="Linux",  
+    reserved=True,  
+    sku=web.SkuDescriptionArgs(
+        tier="Basic",
+        name="B1",
     )
-    .apply(lambda accountKeys: accountKeys.keys[0].value)
 )
 
-pulumi.export("primary_storage_key", primary_key)
+# **9. Web-App erstellen**
+web_app = web.WebApp(
+    "webapp",
+    resource_group_name=resource_group.name,  # Verknüpft mit der Ressourcengruppe
+    location=resource_group.location,  # Gleicher Standort wie die Ressourcengruppe
+    server_farm_id=app_service_plan.id,  # Verknüpft mit dem App Service Plan
+    site_config=web.SiteConfigArgs(  # Konfiguration der Laufzeitumgebung
+        linux_fx_version='PYTHON|3.9',  # Laufzeitumgebung: Python 3.9
+        app_settings=[  # Umgebungsvariablen der Web-App
+            web.NameValuePairArgs(
+                name="WEBSITE_RUN_FROM_PACKAGE",  # App läuft direkt von der ZIP-Datei
+                value=blob_url  # URL zur ZIP-Datei
+            ),
+        ]
+    ),
+    https_only=True  # HTTPS-Zugriff erzwingen
+)
+
+# URL exportieren**
+pulumi.export("web_app_url", Output.concat("http://", web_app.default_host_name))
